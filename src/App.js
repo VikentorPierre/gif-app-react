5a809480f1501ccdb6bc3b21aef4bd48d76dfb97
vikentorpierre <vikentorpierre@gmail.com>
2019-12-14 20:52:19 -0800

    added basename

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ import { Signup, Login } from './Components/auth';
 function App() {
   return (
     <AuthProvider>
-      <Router>
+      <Router basename={window.location.pathname || ''}>
         <div className='App'>
           <Header />
           <Switch>
