79b3f75a13e603e15cfbf05558070c6eef93d773
vikentorpierre <vikentorpierre@gmail.com>
2019-12-14 20:34:22 -0800

    Updates

---
+++ precache-manifest.864da4558d6a284188fddbb897fc98da.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54f31409e2b444f49ac72fe3fe28a110",
+    "url": "/gif-app-react/index.html"
+  },
+  {
+    "revision": "9f71cd4ab1fc12e80faf",
+    "url": "/gif-app-react/static/css/main.06722bd8.chunk.css"
+  },
+  {
+    "revision": "1434765e9ccf74e59ec3",
+    "url": "/gif-app-react/static/js/2.2612c061.chunk.js"
+  },
+  {
+    "revision": "c87d96dbcdc8dd1df6c21457f4947682",
+    "url": "/gif-app-react/static/js/2.2612c061.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9f71cd4ab1fc12e80faf",
+    "url": "/gif-app-react/static/js/main.d8f72e13.chunk.js"
+  },
+  {
+    "revision": "5f3c74af41211341c322",
+    "url": "/gif-app-react/static/js/runtime-main.a4487209.js"
+  }
+]);
